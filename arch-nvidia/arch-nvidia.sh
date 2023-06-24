d8833b037ac89d39ee0d71354296f3f4e4c68a3d
Francabicon <52349593+Francabicon@users.noreply.github.com>
2023-06-24 18:09:07 +0800

    Update arch-nvidia.sh

--- arch-nvidia/arch-nvidia.sh
+++ arch-nvidia/arch-nvidia.sh
@@ -10,4 +10,11 @@ cd nvidia-all
 
 makepkg -si
 
-sudo cp grub /etc/default/grub
+# Specify the new value for GRUB_CMDLINE_LINUX_DEFAULT
+new_value='loglevel=3 quiet rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nvidia-drm.modeset=1'
+
+# Update the GRUB_CMDLINE_LINUX_DEFAULT parameter in the configuration file
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$new_value\"/" /etc/default/grub
+
+# Update the GRUB bootloader configuration
+grub-mkconfig -o /boot/grub/grub.cfg
