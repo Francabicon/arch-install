5deec08662af06f218ad34b1c130a7fbc9be60ab
Francabicon <52349593+Francabicon@users.noreply.github.com>
2023-06-24 18:39:01 +0800

    Update arch-pack.sh

--- arch-pack/arch-pack.sh
+++ arch-pack/arch-pack.sh
@@ -21,8 +21,10 @@ sudo systemctl enable NetworkManager
 
 yay -S linux-zen linux-zen-headers
 
-yay -Sy adobe-source-han-serif-tw-fonts adobe-source-han-serif-hk-fonts adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-hk-fonts adobe-source-han-serif-cn-font adobe-source-han-sans-kr-fonts-2.004-1  adobe-source-han-serif-kr-fonts-2.001-2 adobe-source-han-sans-jp-fonts-2.004-1  adobe-source-han-serif-jp-fonts-2.001
+yay -S adobe-source-han-serif-tw-fonts adobe-source-han-serif-hk-fonts adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-hk-fonts adobe-source-han-serif-cn-font adobe-source-han-sans-kr-fonts-2.004-1  adobe-source-han-serif-kr-fonts-2.001-2 adobe-source-han-sans-jp-fonts-2.004-1  adobe-source-han-serif-jp-fonts-2.001
 
 yay -S bash-completion python-click-completion git-completion ntfs-dkms
 
 yay -S brave firefox-developer-edition torbrowser-launcher
+
+yay -Syyu
