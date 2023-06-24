0fdae34ecfad35fd7cee74629c82a9c207b5e8b4
Francabicon <52349593+Francabicon@users.noreply.github.com>
2023-06-24 18:34:37 +0800

    Update arch-kde-config.sh

--- arch-kde-config/arch-kde-config.sh
+++ arch-kde-config/arch-kde-config.sh
@@ -3,6 +3,6 @@
 
 yay -Sy
 
-yay -S kdegraphics-thumbnailers kamersa kdegraphics-mobipocket gwenview kcal packagekit-qt5 printer-manager kde-service-menu-pdf system side kde-servicemenus-rootactions kamera ktorrent kcalc 
+yay -S kdegraphics-thumbnailers kdegraphics-mobipocket gwenview kcal packagekit-qt5 printer-manager kde-service-menu-pdf system side kde-servicemenus-rootactions kamera ktorrent kcalc 
 
 
